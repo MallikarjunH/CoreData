9b9cd8587f37b6b7eda6249c07999c671ebf4a07
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-08-20 12:52:56 +0530

    code update

--- CoreData_KiloLoco/CoreData_KiloLoco/AppDelegate.swift
+++ CoreData_KiloLoco/CoreData_KiloLoco/AppDelegate.swift
@@ -18,7 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let vc = ViewController()
+        let vc = ViewController(persistentManager: PersistenceManager.shared)
+        
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
--- CoreData_KiloLoco/CoreData_KiloLoco/ViewController.swift
+++ CoreData_KiloLoco/CoreData_KiloLoco/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let persistentManager: PersistenceManager
+    
+    init(persistentManager: PersistenceManager){
+        self.persistentManager =  persistentManager
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
       //  print("hello")
+        
+        persistentManager.saveContext()
         
     }
 
