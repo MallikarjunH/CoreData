d9f1636b84cf6d3246a0efcc3faf269fdbe41b30
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-09-30 16:04:54 +0530

    fetch data - added

--- CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
@@ -2,14 +2,15 @@
 //  ViewController.swift
 //  CoreData_YogeshPatel
 //
-//  Created by mallikarjun on 27/09/19.
-//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//  Created by Nikits on 27/09/19.
+//  Copyright © 2019 Nikita H. All rights reserved.
 //
 
 //Video 1: https://www.youtube.com/watch?v=JTtYSJtVKMc
 
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
@@ -32,6 +33,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let dict = ["name":nameTextField.text,"email":emailTextField.text,"mobile":mobileTextField.text,"state":stateTextField.text]
         
         DatabaseHelper.shareInstance.save(object: dict as! [String : String])
+        
+        //print("Document Directory :",FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found")
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        //request.predicate = NSPredicate(format: "age = %@", "12")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+               print(data.value(forKey: "name") as! String)
+          }
+            
+        } catch {
+            
+            print("Failed")
+        }
+        
     }
     
     //MARK: TextField Delagate mthods
