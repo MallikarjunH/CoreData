9567f58301f9816f0bac4cd4519f9cfcdf060eca
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-09-30 09:57:43 +0530

    Data module added

--- CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2019 Mallikarjun H. All rights reserved.
 //
 
+//Video 1: https://www.youtube.com/watch?v=JTtYSJtVKMc
+
+
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
@@ -23,6 +26,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
 
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let dict = ["name":nameTextField.text,"email":emailTextField.text,"mobile":mobileTextField.text,"state":stateTextField.text]
+        
+        DatabaseHelper.shareInstance.save(object: dict as! [String : String])
+    }
     
     //MARK: TextField Delagate mthods
     
---
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Model/DatabaseHelper.swift
@@ -0,0 +1,37 @@
+//
+//  DatabaseHelper.swift
+//  CoreData_YogeshPatel
+//
+//  Created by mallikarjun on 27/09/19.
+//  Copyright © 2019 Mallikarjun H. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class DatabaseHelper{
+    
+    static var shareInstance = DatabaseHelper() //it is required if you want to access following method outside any class
+    
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    func save(object:[String:String]){
+        
+        let student = NSEntityDescription.insertNewObject(forEntityName: "Student", into: context!) as! Student
+        
+        student.name = object["name"]
+        student.email = object["email"]
+        student.mobile = object["mobile"]
+        student.state = object["state"]
+        
+        do{
+            
+            try context?.save()
+        }catch{
+          print("daata is not save")
+        }
+    
+    }
+    
+}
