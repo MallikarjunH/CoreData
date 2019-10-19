a63278a6aea49dc4700af5ce11bf73b7b72ff8f2
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-10-19 10:12:54 +0530

    core removed

--- CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
@@ -13,87 +13,13 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITextFieldDelegate, DataPass {
+class ViewController: UIViewController {
 
-
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var mobileTextField: UITextField!
-    @IBOutlet weak var stateTextField: UITextField!
-    
-    var i = Int()
-    var isUpdate = Bool()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-    
-
-    
-    @IBAction func saveButtonClicked(_ sender: Any) {
-        
-        let dict = ["name":nameTextField.text,"email":emailTextField.text,"mobile":mobileTextField.text,"state":stateTextField.text]
-        
-        DatabaseHelper.shareInstance.save(object: dict as! [String : String])
-        
-        if isUpdate{
-            
-            DatabaseHelper.shareInstance.editData(object: dict as! [String:String], i: i)
-        }else{
-            
-            DatabaseHelper.shareInstance.save(object: dict as! [String:String])
-        }
-        //print("Document Directory :",FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found")
-        
-      /*  let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
-        request.returnsObjectsAsFaults = false
-       
-        do {
-            let result = try context.fetch(request)
-            for data in result as! [NSManagedObject] {
-               print(data.value(forKey: "name") as! String)
-          }
-            
-        } catch {
-            
-            print("Failed")
-        } */
-        
-    }
-    
-    //Mark: Confirm The Protocol
-    
-    func data(object: [String : String], index: Int, isEdit: Bool) {
-          
-                nameTextField.text = object["name"]
-                emailTextField.text = object["email"]
-                mobileTextField.text = object["mobile"]
-                stateTextField.text = object["state"]
-                i = index
-                isUpdate = isEdit
-       }
-    
-    
-   // ViewSavedDataVC //
-    @IBAction func showDataButtonClicked(_ sender: Any) {
-        
-        let detailsVC = self.storyboard?.instantiateViewController(identifier: "ViewSavedDataVCId") as! ViewSavedDataVC
-        detailsVC.delegate = self
-        self.navigationController?.pushViewController(detailsVC, animated: true)
-    }
-    
-    
-    //MARK: TextField Delagate mthods
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
-        textField.resignFirstResponder()
-        
-        return true
     }
 
 }
--- CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewSavedDataVC.swift
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewSavedDataVC.swift
@@ -8,80 +8,18 @@
 
 import UIKit
 
-protocol DataPass {
-    
-    func data(object: [String:String], index: Int, isEdit: Bool)
-}
 
-class ViewSavedDataVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+class ViewSavedDataVC: UIViewController {
     
-    @IBOutlet weak var mainTableView: UITableView!
-    
-    //creating delegate
-    var delegate:DataPass!
-    
-    var student  = [Student]()
-    
+     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        student = DatabaseHelper.shareInstance.getStudentData()
-    }
-    
-     // MARK: - TableViee DatSource and Deleagte Methods
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return student.count
-        //return DatabaseHelper.shareInstance.getStudentData().count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCellId", for: indexPath) as! TableViewCell
-        
-     /*   cell.nameLabel.text = student[indexPath.row].name
-        cell.emailLabel.text = student[indexPath.row].email
-        cell.mobileLabel.text = student[indexPath.row].mobile
-        cell.stateLabel.text = student[indexPath.row].state */
-        
-        //using computed property - code written in TableViewCell - So top code not need
-        cell.student = student[indexPath.row]
-        
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        let dict = ["name":student[indexPath.row].name, "email":student[indexPath.row].email,"mobile":student[indexPath.row].mobile,"state":student[indexPath.row].state]
-        delegate.data(object: dict as! [String: String], index: indexPath.row, isEdit: true)
-        self.navigationController?.popViewController(animated: true)
-    }
-    
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        
-        return true
+       
     }
 
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete{
-            student = DatabaseHelper.shareInstance.deleteData(index: indexPath.row)
-            self.mainTableView.deleteRows(at: [indexPath], with: .automatic)
-        }
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
