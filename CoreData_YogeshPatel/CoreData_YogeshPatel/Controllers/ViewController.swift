2e691e0c833b8f8592f966bb20e6832a7e9e863d
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-10-19 11:25:46 +0530

    Save Data - completed

--- CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Controllers/ViewController.swift
@@ -26,11 +26,37 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    //MARK: Save data
     @IBAction func saveDataButtonClicked(_ sender: Any) {
     
+        let dict = ["name1":nameTextField.text,"address1":addressTextField.text,"city1":cityTextField.text,"mobile1":mobileTextField.text]
+        
+        DatabaseHelper.sharedInstance.save(object: dict as! [String : String])
+        
+        //print("Document Directory :",FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found")
+          
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+          
+          let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+          request.returnsObjectsAsFaults = false
+         
+          do {
+              let result = try context.fetch(request)
+              for data in result as! [NSManagedObject] {
+                 print(data.value(forKey: "name") as! String)
+            }
+              
+          } catch {
+              
+              print("Failed")
+          } 
+        
     }
     
+    //MARK: Show Data
     @IBAction func showDataButtonClicked(_ sender: Any) {
+        
+        
   
     }
     
--- CoreData_YogeshPatel/CoreData_YogeshPatel/Model/DatabaseHelper.swift
+++ CoreData_YogeshPatel/CoreData_YogeshPatel/Model/DatabaseHelper.swift
@@ -11,3 +11,27 @@ import UIKit
 import CoreData
 
 
+class DatabaseHelper{
+    
+    //Creating a instance - in order to access following fuctions to other classes
+    static var sharedInstance = DatabaseHelper()
+    
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    func save(object:[String:String]){
+        
+        let student = NSEntityDescription.insertNewObject(forEntityName: "Student", into: context!) as! Student
+        
+         student.name = object["name1"]
+         student.address = object["address1"]
+         student.city = object["city1"]
+         student.mobile = object["mobile1"]
+        
+        do{
+            try context?.save()
+        }catch{
+            
+            print("Data is not saved")
+        }
+    }
+}
